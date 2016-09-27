d1d001900b6837343027b611a74008300bd53b0c
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2016-09-27 11:54:07 -0500

    Updated for current run implementation

--- example-run-espa-cmd.sh
+++ example-run-espa-cmd.sh
@@ -1,21 +1,20 @@
 docker run --rm --tty \
-  --cap-add LINUX_IMMUTABLE \
-  --hostname $USER-cli \
-  --name $USER-cli \
-  --volume <HOST>/work-dir:/home/espa/work-dir:rw \
-  --volume <HOST>/output-data:/home/espa/output-data:rw \
-  --volume <HOST>/auxiliaries:/usr/local/auxiliaries:ro \
-  --volume <HOST>/input-data:/home/espa/input-data:ro \
-  --volume $HOME/.usgs:/home/espa/.usgs:ro \
-  --workdir /home/espa \
-  espa.centos.science:b2.11.4 `id -g` `id -u` \
+  --user `id -u`:`id -g` \
+  --hostname ${USER}-cli \
+  --name ${USER}-cli \
+  --volume <HOST_PATH_TO>/work-dir:/home/espa/work-dir:rw \
+  --volume <HOST_PATH_TO>/output-data:/home/espa/output-data:rw \
+  --volume <HOST_PATH_TO>/auxiliaries:/usr/local/auxiliaries:ro \
+  --volume <HOST_PATH_TO>/input-data:/home/espa/input-data:ro \
+  --volume ${HOME}/.usgs:/home/espa/.usgs:ro \
+  --workdir /home/espa/work-dir \
+  espa.centos.science:b2.11.4 \
   cli.py \
-    --order-id frog \
-    --input-product-id LE70390372003148EDC00 \
+    --order-id toaster \
+    --input-product-id LE07_L1TP_029029_20160728_20160831_01_A1 \
     --product-type landsat \
     --output-format envi \
-    --input-url file:///home/espa/input-data/LE7/LE70390372003148EDC00.tar.gz \
+    --input-url file:///home/espa/input-data/LE07/LE07_L1TP_029029_20160728_20160831_01_A1.tar.gz \
     --include-top-of-atmosphere \
     --include-brightness-temperature \
-    --include-surface-reflectance \
-    --include-cfmask \
+    --include-surface-reflectance
